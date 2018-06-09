8eb181cb381d0af889f3ecf6a20620fbbd30c8ee
Marcel Schouwenaar <marcel@the-incredible-machine.com>
2018-06-08 23:24:10 -0400

    attempt to filter out USB

--- ev3_scratch.js
+++ ev3_scratch.js
@@ -1367,10 +1367,9 @@ function(ext)
          // the Scratch plugin is only letting us know about serial ports with names that
          // "begin with tty.usbmodem, tty.serial, or tty.usbserial" - according to khanning
 
-
-
          
-         if ((dev.id.indexOf('/dev/tty.serial') === 0 && dev.id.indexOf('-SerialPort') != -1) || dev.id.indexOf('COM') === 0)
+         // if ((dev.id.indexOf('/dev/tty.serial') === 0 && dev.id.indexOf('-SerialPort') != -1) || dev.id.indexOf('COM') === 0)
+         if (dev.id.indexOf('/dev/tty.serial-SerialPort') === 0)
          {
          
              if (potentialEV3Devices.filter(function(e) { return e.id == dev.id; }).length == 0)
