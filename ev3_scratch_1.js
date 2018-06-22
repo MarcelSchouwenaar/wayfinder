1c497b37bca8e4c825b036b7ba2da713dbb89482
Marcel Schouwenaar <marcel@the-incredible-machine.com>
2018-06-21 23:48:31 -0400

    v12b

--- ev3_scratch_1.js
+++ ev3_scratch_1.js
@@ -4,7 +4,7 @@
 // Rename the brick before pairing it with the Mac or else the name gets cached and the serial port will have the old name
 // My bricks are named serialBrick1 (etc)
 // Turn off the iPod/iPhone/iPad checkbox on the EV3 Bluetooth settings after pairing or else it will not work at all
-var version = "v12  ";
+var version = "v12b  ";
 
 
 function timeStamp()
@@ -654,8 +654,7 @@ function receive_handler(data)
     {
        theResult = getFloatResult(inputData);
        port = 3;
-       // theResult = 50;
-       console.log(">> detected the temperature sensor", theResult, port, type, mode, callback, theCommand);
+       console.log(">> detected the temperature sensor", theResult, "port", port, type, mode, callback, theCommand);
 
     }
 
